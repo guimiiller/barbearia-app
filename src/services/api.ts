c471d1fb2065bc961ea3ac566745c3ea49320369
guimiiller <guilhermemillerblack@gmail.com>
2026-08-23 11:26:29 -0300

    chore: configure production API endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://192.168.0.6:3000",
+  baseURL: "https://barao-barbearia-api.onrender.com",
 });
 
 export type AppointmentData = {
