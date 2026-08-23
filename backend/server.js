d534bc3041f79aeb39f69c7c2b3f4a1b7778c33a
guimiiller <guilhermemillerblack@gmail.com>
2026-08-23 10:47:43 -0300

    chore: prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,8 @@ app.use("/appointments", appointmentRoutes);
 app.use("/users", userRoutes);
 app.use("/schedule", scheduleRoutes);
 
-app.listen(3000, () => {
-  console.log("🚀 Servidor rodando na porta 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
