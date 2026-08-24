04efc4a84c22cc9eaa2ba4f196e517982eb742f4
guimiiller <guilhermemillerblack@gmail.com>
2026-08-23 22:47:49 -0300

    feat: add PWA manifest and install icons

---
+++ app/+html.tsx
@@ -0,0 +1,33 @@
+import { ScrollViewStyleReset } from "expo-router/html";
+import type { PropsWithChildren } from "react";
+
+export default function Root({ children }: PropsWithChildren) {
+  return (
+    <html lang="pt-BR">
+      <head>
+        <meta charSet="utf-8" />
+
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+
+        <meta name="theme-color" content="#0B0B0B" />
+
+        {/* PWA */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* Ícones */}
+        <link rel="icon" type="image/png" href="/icon-192.png" />
+
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+
+        <ScrollViewStyleReset />
+      </head>
+
+      <body>{children}</body>
+    </html>
+  );
+}
